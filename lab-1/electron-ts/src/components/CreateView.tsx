102bba486bd257716219178bc5385a565a102ac8
nichitaa <nichitaafk@gmail.com>
2021-09-21 11:10:23 +0300

    code clean up + switched to contextAPI and useReducer

--- lab-1/electron-ts/src/components/CreateView.tsx
+++ lab-1/electron-ts/src/components/CreateView.tsx
@@ -1,30 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Col, Input, Row, Switch, Typography} from "antd";
 import {CustomPolicyCard} from "./CustomPolicyCard";
 import {objectClone, showMessage} from "../utils";
 import {insertAuditDocument} from "../services/api";
+import {AppContext} from "../context/context";
+import {toggleIsEditView} from "../context/reducer";
+import {IAuditCustomItem, IAuditDocument} from "../types";
 
 const {Search} = Input;
 const {Title} = Typography;
 
+const CreateView = () => {
 
-const CreateView = (props) => {
+    const {state, dispatch} = useContext(AppContext)
+    const {editViewItem} = state
 
-    const [auditContent, setAuditContent] = useState<object>({});
-    const [newAuditDocumentName, setNewAuditDocumentName] = useState('');
-    const [filteredCustomItems, setFilteredCustomItems] = useState<object[]>([]);
-    const [initialCustomItems, setInitialCustomItems] = useState<object[]>([]);
-    const [searchValue, setSearchValue] = useState('');
+    const [auditContent, setAuditContent] = useState<IAuditDocument>({});
+    const [newAuditDocumentName, setNewAuditDocumentName] = useState<string>('');
+    const [filteredCustomItems, setFilteredCustomItems] = useState<IAuditCustomItem[]>([]);
+    const [initialCustomItems, setInitialCustomItems] = useState<IAuditCustomItem[]>([]);
+    const [searchValue, setSearchValue] = useState<string>('');
 
     useEffect(() => {
-        const clone = objectClone(props.editViewItem);
+        const clone = objectClone(editViewItem);
         setAuditContent(clone);
-
-        const temp: object[] = [];
+        const temp: IAuditCustomItem[] = [];
         for (let el of clone.audit_custom_items) temp.push({...el, isActive: true});
         setFilteredCustomItems(temp);
         setInitialCustomItems(temp);
-    }, [props.editViewItem]);
+    }, [editViewItem]);
 
     const onSearchChange = (search) => {
         const searchString = search.toLowerCase();
@@ -38,12 +42,10 @@ const CreateView = (props) => {
                 for (let key in el) {
                     if (key !== 'isActive') {
                         const value = el[key].toLowerCase();
-                        if (value.includes(searchString)) {
-                            includes = true;
-                        }
+                        if (value.includes(searchString)) includes = true;
                     }
                 }
-                if(includes) temp.push(el)
+                if (includes) temp.push(el)
                 return el;
             });
             setFilteredCustomItems(prev => temp);
@@ -61,9 +63,9 @@ const CreateView = (props) => {
     }
 
     const updateSinglePolicyActiveState = (bool, idx) => {
-        let item = {}
+        let item: IAuditCustomItem = {}
         setFilteredCustomItems(prev => prev.map((el, index) => {
-            if (idx === index) { // @ts-ignore
+            if (idx === index) {
                 el.isActive = bool;
                 item = el;
             }
@@ -76,7 +78,6 @@ const CreateView = (props) => {
 
         setInitialCustomItems(prev => prev.map(el => {
             if (areSameObjects(el, item)) {
-                // @ts-ignore
                 el.isActive = item.isActive
             }
             return el;
@@ -88,21 +89,19 @@ const CreateView = (props) => {
             showMessage('error', 'please enter new audit document name', 2);
         } else {
             const body = auditContent;
-            const activePolicies = filteredCustomItems.filter(el => {
-                // @ts-ignore
+            const activePolicies = filteredCustomItems.filter((el: IAuditCustomItem) => {
                 if (el.isActive) return el;
             });
-            body['audit_custom_items'] = [...activePolicies];
-            body['audit_filename'] = newAuditDocumentName + '.audit';
-            delete body['audit_file']
+            body.audit_custom_items = [...activePolicies];
+            body.audit_filename = newAuditDocumentName + '.audit';
+            delete body.audit_file
             console.log('current configuration: ', body);
             insertAuditDocument(body).then(res => {
                 console.log('[insertAuditDocument] response: ', res);
                 if (res.isSuccess) {
-                    props.toggleIsEditView(false);
+                    dispatch(toggleIsEditView(false))
                     showMessage('success', 'new audit configuration successfully saved', 1);
-                }
-                else showMessage('error', res.error, 2);
+                } else showMessage('error', res.error, 2);
             })
                 .catch(err => console.log("error: ", err))
         }
@@ -112,68 +111,66 @@ const CreateView = (props) => {
         if (e.target.value.trim().length === e.target.value.length) setNewAuditDocumentName(prev => e.target.value)
     }
 
-    return (
-        <div>
-            <Row gutter={[8, 8]} style={{marginBottom: '10px'}}>
-                <Col span={4}>
-                    <Button danger={true} type={'dashed'} style={{width: '100%'}}
-                            onClick={() => props.toggleIsEditView(false)}>{'< Back'}</Button>
-                </Col>
-                <Col style={{textAlign: 'center'}} offset={9} span={6}>
-                    <Switch checkedChildren={'all policies active'}
-                            unCheckedChildren={'all policies disabled'}
-                            defaultChecked={true}
-                            onChange={bool => togglePolicyActiveState(bool)}/>
-                </Col>
-                <Col span={5}>
-                    <Button style={{width: '100%'}} onClick={saveCurrentConfiguration}>
-                        Save
-                    </Button>
-                </Col>
-            </Row>
+    return <>
+        <Row gutter={[8, 8]} style={{marginBottom: '10px'}}>
+            <Col span={4}>
+                <Button danger={true} type={'dashed'} style={{width: '100%'}}
+                        onClick={() => dispatch(toggleIsEditView(false))}>{'< Back'}</Button>
+            </Col>
+            <Col style={{textAlign: 'center'}} offset={9} span={6}>
+                <Switch checkedChildren={'all policies active'}
+                        unCheckedChildren={'all policies disabled'}
+                        defaultChecked={true}
+                        onChange={bool => togglePolicyActiveState(bool)}/>
+            </Col>
+            <Col span={5}>
+                <Button style={{width: '100%'}} onClick={saveCurrentConfiguration}>
+                    Save
+                </Button>
+            </Col>
+        </Row>
 
-            <Row style={{marginBottom: '10px'}} gutter={[8, 8]}>
-                <Col span={12}>
-                    <Input
-                        placeholder={'Enter a new audit document name'}
-                        value={newAuditDocumentName}
-                        onChange={handleChangeAuditDocumentName}
-                        onKeyDown={(e) => {
-                            if(e.keyCode===13) saveCurrentConfiguration();
-                        }}
-                    />
-                </Col>
-                <Col span={12}>
-                    <Search
-                        placeholder="find a policy by a keyword"
-                        allowClear
-                        enterButton="Search"
-                        size="middle"
-                        value={searchValue}
-                        onChange={(e) => onSearchChange(e.target.value)}
-                        onSearch={(val) => onSearchChange(val)}
-                    />
-                </Col>
-                <Title
-                    style={{display: 'inline-block', color: '#645790', fontSize: '15px'}}
-                    level={5}>
-                    <code>found [{filteredCustomItems.length}] 📑 policies</code>
-                </Title>
-            </Row>
+        <Row style={{marginBottom: '10px'}} gutter={[8, 8]}>
+            <Col span={12}>
+                <Input
+                    placeholder={'Enter a new audit document name'}
+                    value={newAuditDocumentName}
+                    onChange={handleChangeAuditDocumentName}
+                    onKeyDown={(e) => {
+                        if (e.keyCode === 13) saveCurrentConfiguration();
+                    }}
+                />
+            </Col>
+            <Col span={12}>
+                <Search
+                    placeholder="find a policy by a keyword"
+                    allowClear
+                    enterButton="Search"
+                    size="middle"
+                    value={searchValue}
+                    onChange={(e) => onSearchChange(e.target.value)}
+                    onSearch={(val) => onSearchChange(val)}
+                />
+            </Col>
+            <Title
+                style={{display: 'inline-block', color: '#645790', fontSize: '15px'}}
+                level={5}>
+                <code>found [{filteredCustomItems.length}] 📑 policies</code>
+            </Title>
+        </Row>
 
-            <Row gutter={[8, 8]}>
-                {filteredCustomItems.map((item, index) => {
-                    return <Col key={index} span={filteredCustomItems.length === 1 ? 24 : 12}>
-                        <CustomPolicyCard updateSinglePolicyActiveState={updateSinglePolicyActiveState} idx={index}
-                                          policy={item}/>
-                    </Col>
-                })}
-                {filteredCustomItems.length === 0
-                && <p>Could not find any matching audit policy</p>}
-            </Row>
-
-        </div>
-    );
+        <Row gutter={[8, 8]}>
+            {filteredCustomItems.map((item, index) => {
+                return <Col key={index} span={filteredCustomItems.length === 1 ? 24 : 12}>
+                    <CustomPolicyCard
+                        updateSinglePolicyActiveState={updateSinglePolicyActiveState}
+                        idx={index}
+                        policy={item}/>
+                </Col>
+            })}
+            {filteredCustomItems.length === 0 && <p>Could not find any matching audit policy</p>}
+        </Row>
+    </>
 };
 
 export default CreateView;
---
+++ lab-1/electron-ts/src/context/context.ts
@@ -0,0 +1,52 @@
+import React from 'react';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import {AppState, InitialAppState} from "./state";
+
+const middlewares = [thunk, logger];
+
+export const useMiddlewareReducer = createUseMiddlewareReducer(middlewares);
+
+
+
+// A middleware has type (dispatch, getState) => nextMw => action => action
+function enhanceDispatch({ getState, stateDispatch }) {
+    return (...middlewares) => {
+        let dispatch;
+        const middlewareAPI = {
+            getState,
+            dispatch: action => dispatch(action)
+        };
+        dispatch = middlewares
+            .map(m => m(middlewareAPI))
+            .reduceRight((next, mw) => mw(next), stateDispatch);
+        return dispatch;
+    };
+}
+
+function createUseMiddlewareReducer(middlewares) {
+    return (reducer, initState, initializer = s => s) => {
+        const [state, setState] = React.useState(initializer(initState));
+        const stateRef = React.useRef(state); // stores most recent state
+        const dispatch = React.useMemo(
+            () =>
+                enhanceDispatch({
+                    getState: () => stateRef.current, // access most recent state
+                    stateDispatch: action => {
+                        stateRef.current = reducer(stateRef.current, action); // makes getState() possible
+                        setState(stateRef.current); // trigger re-render
+                        return action;
+                    }
+                })(...middlewares),
+            [middlewares, reducer]
+        );
+        return [state, dispatch];
+    }
+}
+
+
+
+export const AppContext = React.createContext<{state: AppState, dispatch: () => undefined}>({
+    state: InitialAppState,
+    dispatch: () => undefined
+})
