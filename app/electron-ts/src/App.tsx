33fd98cc22fc4c0f7948b75cfc750152221a7fd4
Pasecinic Nichita <nichitastrix@gmail.com>
2021-11-15 21:22:54 +0200

    added google / github and twitter as oauth providers

--- app/electron-ts/src/App.tsx
+++ app/electron-ts/src/App.tsx
@@ -1,28 +1,63 @@
-import {useContext, useEffect} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import './App.css';
-import {ParsedView} from "./components/ParsedView";
-import {DocumentList} from "./components/DocumentList";
-import CreateView from "./components/CreateView";
-import {AppContext} from "./context/context";
-import {fetchData} from "./context/reducer";
+import {ParsedView} from './components/ParsedView';
+import {DocumentList} from './components/DocumentList';
+import CreateView from './components/CreateView';
+import {AppContext} from './context/context';
+import {fetchData} from './context/reducer';
+import {Button} from 'antd';
+import {getUserData} from './services/api';
 
 const App = () => {
 
-    const {state, dispatch} = useContext(AppContext)
+    const {state, dispatch} = useContext(AppContext);
+    const [user, setUser] = useState<null | any>(null);
     const {isParseView, isEditView} = state;
 
     useEffect(() => {
-        dispatch(fetchData())
+        dispatch(fetchData());
+        const getUser = async () => {
+            const res = await getUserData();
+            if (res.user) setUser(res.user);
+            console.log('user data: ', res.user);
+        };
+        getUser();
     }, [dispatch]);
+
+    const googleLogin = () => {
+        window.open('http://localhost:8080/api/auth/google', '_self');
+    };
+
+    const githubLogin = () => {
+        window.open('http://localhost:8080/api/auth/github', '_self');
+    };
+
+    const twitterLogin = () => {
+        window.open('http://localhost:8080/api/auth/twitter', '_self');
+    };
+
+    const appLogout = () => {
+        window.open('http://localhost:8080/api/auth/logout', '_self');
+    };
 
     return (
         <div className="App">
+            {user === null
+            && <>
+                <Button onClick={googleLogin}>Google Login</Button>
+                <Button onClick={githubLogin}>Github Login</Button>
+                <Button onClick={twitterLogin}>Twitter Login</Button>
+            </>}
+            {user !== null
+            && <Button onClick={appLogout}>Logout</Button>}
+
             {/* MAIN VIEW */}
             {(!isParseView && !isEditView) && <DocumentList/>}
             {/* FILE PARSED VIEW MODE */}
             {isParseView && <ParsedView/>}
             {/* FILE EDIT MODE */}
             {isEditView && <CreateView/>}
+
         </div>
     );
 
--- app/electron-ts/src/services/api.ts
+++ app/electron-ts/src/services/api.ts
@@ -8,66 +8,83 @@ const regeditApi = axios.create({
     baseURL: 'http://localhost:8080/api/regedit'
 });
 
+const authApi = axios.create({
+    baseURL: 'http://localhost:8080/api/auth'
+});
+
+export const getUserData = async () => {
+    return authApi.get('/login/success', {
+        withCredentials: true,
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': true
+        }
+    })
+        .then(res => res.data)
+        .catch(err => err.message);
+};
+
 export const policyBatchFix = async (items) => {
     return await regeditApi.post('/batch_enforce', {items})
         .then(res => res.data)
-        .catch(err => err.message)
-}
+        .catch(err => err.message);
+};
 
 export const backupRegistry = async () => {
     return await regeditApi.post('/backup')
         .then(res => res.data)
-        .catch(err => err.message)
-}
+        .catch(err => err.message);
+};
 
 export const enforcePolicyItem = async (item) => {
     return await regeditApi.post('/enforce', item)
         .then(res => res.data)
-        .catch(err => err.message)
-}
+        .catch(err => err.message);
+};
 
 export const testCustomItem = async (item) => {
     // const body = {data: JSON.stringify(item)}
     return await regeditApi.post('/', item)
         .then(res => res.data)
-        .catch(err => err.message)
-}
+        .catch(err => err.message);
+};
 
 export const fetchAuditDocuments = async () => {
     return await api.get('/')
         .then(res => res.data)
         .catch(err => console.error(err.message));
-}
+};
 
 export const checkUniqueDocument = async (filename) => {
     return await api.get('/exists/' + filename)
         .then(res => res.data)
-        .catch(err => err.message)
-}
+        .catch(err => err.message);
+};
 
 export const uploadAuditDocument = async (formBody) => {
     return await api.post('/', formBody)
         .then(res => res.data)
         .catch(err => err.message);
-}
+};
 
 export const downloadAuditFile = async (filename) => {
     return await api.get('/download/' + filename, {
         responseType: 'blob'
     }).then(res => res.data)
         .catch(err => err.message);
-}
+};
 
 export const deleteAuditDocument = async (filename) => {
     return await api.delete('/' + filename)
         .then(res => res.data)
         .catch(err => err.message);
-}
+};
 
 export const insertAuditDocument = async (body) => {
     return await api.post('/insert', body)
         .then(res => res.data)
         .catch(err => err);
-}
+};
 
 export default api;
